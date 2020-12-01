1a274a601ac38188c850d0df91e8fad9933bfc0e
DoubleC <cuichuang_work@163.com>
2020-12-01 18:12:39 +0800

    http

--- Node学习/path模块/时钟案例.js
+++ Node学习/path模块/时钟案例.js
@@ -11,7 +11,7 @@ fs.readFile(path.join(__dirname, './/index.html'), 'utf-8', function(err, dataSt
     // \s\S任意字符， * 多次
     var regstyle = /<style>[\s\S]*<\/style>/
     var regscript = /<script>[\s\S]*<\/script>/
-    source = resolve(source, regstyle, 'style', 'css', '<link rel="stylesheet" href="./index.css" />')
+    source = resolve(source, regstyle, 'style', 'css', '<link rel="stylesheet" href="./index.css"/>')
 
     resolve(source, regscript, 'script', 'js', '<script src="./index.js"></script>')
 })
